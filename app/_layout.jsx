1918c61b981684cbfc08889678339220ebc42cbe
AdnanTemur <adt.gb001@gmail.com>
2024-09-26 01:27:14 +0500

    first

---
+++ app/_layout.jsx
@@ -0,0 +1,60 @@
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+import "react-native-reanimated";
+import { StyleSheet, View } from "react-native";
+import { WebView } from "react-native-webview";
+
+import { useColorScheme } from "@/hooks/useColorScheme";
+
+// Prevent the splash screen from auto-hiding before asset loading is complete.
+SplashScreen.preventAutoHideAsync();
+
+export default function RootLayout() {
+  const colorScheme = useColorScheme();
+  const [loaded] = useFonts({
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+  });
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+
+  return (
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+      {/* <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="+not-found" />
+      </Stack> */}
+      <View style={styles.container}>
+        <WebView
+          source={{ uri: "https://ecolumen.vercel.app/" }}
+          style={styles.webview}
+        />
+      </View>
+    </ThemeProvider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 30,
+    paddingBottom: 30,
+  },
+  webview: {
+    flex: 1,
+  },
+});
